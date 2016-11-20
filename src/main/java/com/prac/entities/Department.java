f6f9e5a4ebd7a453a2393ece7b2d3c94675299f4
Revanth <revanthbomma@gmail.com>
2016-11-19 18:23:00 -0600

    Initial commit

---
+++ src/main/java/com/prac/entities/Department.java
@@ -0,0 +1,7 @@
+package com.prac.entities;
+
+/**
+ * Created by revan on 11/19/2016.
+ */
+public class Department {
+}
---
+++ src/main/java/com/prac/repositories/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.prac.repositories;
+
+/**
+ * Created by revan on 11/19/2016.
+ */
+public interface DepartmentRepository {
+}
